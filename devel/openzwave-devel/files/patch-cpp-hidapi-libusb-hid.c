8c3cd3c2f1adef53d58824e486d9888910bff5d3
Johan Ström <johan@stromnet.se>
2015-11-25 20:05:24 +0100

    Updated to latest openzwave

--- devel/openzwave-devel/files/patch-cpp-hidapi-libusb-hid.c
+++ devel/openzwave-devel/files/patch-cpp-hidapi-libusb-hid.c
@@ -1,11 +1,14 @@
---- cpp/hidapi/libusb/hid.c.orig	2015-11-25 07:33:13.805753000 +0000
-+++ cpp/hidapi/libusb/hid.c	2015-11-25 07:33:38.030362000 +0000
-@@ -373,7 +373,7 @@
- 	inbytes = len-2;
- 	outptr = (char*) wbuf;
- 	outbytes = sizeof(wbuf);
--	res = iconv(ic, &inptr, &inbytes, &outptr, &outbytes);
-+	res = iconv(ic, (char**)&inptr, &inbytes, &outptr, &outbytes);
- 	if (res == (size_t)-1) {
- 		LOG("iconv() failed\n");
- 		goto err;
+--- cpp/hidapi/libusb/hid.c
++++ cpp/hidapi/libusb/hid.c
+@@ -335,11 +335,7 @@ static wchar_t *get_usb_string(libusb_device_handle *dev, uint8_t idx)
+ 	size_t inbytes;
+ 	size_t outbytes;
+ 	size_t res;
+-#ifdef __FreeBSD__
+-	const char *inptr;
+-#else
+ 	char *inptr;
+-#endif
+ 	char *outptr;
+ 
+ 	/* Determine which language to use. */
