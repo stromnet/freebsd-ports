972805c54c6536f6925f9de7c359a4f87b635c56
Johan Ström <johan@stromnet.se>
2016-01-21 21:06:15 +0100

    Updated openzwave-devel to current HEAD

--- devel/openzwave-devel/files/patch-cpp-hidapi-libusb-hid.c
+++ devel/openzwave-devel/files/patch-cpp-hidapi-libusb-hid.c
@@ -1,14 +1,11 @@
 --- cpp/hidapi/libusb/hid.c
 +++ cpp/hidapi/libusb/hid.c
-@@ -335,11 +335,7 @@ static wchar_t *get_usb_string(libusb_device_handle *dev, uint8_t idx)
+@@ -335,7 +335,7 @@ static wchar_t *get_usb_string(libusb_device_handle *dev, uint8_t idx)
  	size_t inbytes;
  	size_t outbytes;
  	size_t res;
--#ifdef __FreeBSD__
 -	const char *inptr;
--#else
- 	char *inptr;
--#endif
++	char *inptr;
  	char *outptr;
  
  	/* Determine which language to use. */
